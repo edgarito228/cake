8d8dcc4159443c7f99e70d0ec20b0eeb239c2518
edgarito228 <trofedgar17@gmail.com>
2024-12-10 10:04:59 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        pech();
+
+
+
+    }
+    public static void testo(){
+        System.out.println("Mix the dry ingredients.");
+        System.out.println("Cream the butter and sugar.");
+        System.out.println("Beat in the eggs.");
+        System.out.println("Stir in the dry ingredients.");
+
+    }
+    public static void pech(){
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+        ukrashat();
+    }
+    public static void ukrashat() {
+        System.out.println("Mix ingredients for frosting.");
+        System.out.println("Spread frosting and sprinkles.");
+    }
+}
